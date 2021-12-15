c792ef0741b43835dba396a9702273121fb8d456
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-12-15 16:43:08 +0900

    feat: Add onSocialClick func

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,10 @@
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  getAuth,
+  signInWithPopup,
 } from "firebase/auth";
 import React, { useState } from "react";
 import { authService } from "fbase";
@@ -9,7 +12,7 @@ import { authService } from "fbase";
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(false);
+  const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState("");
   /*input에 값이 입력될 때 onChange function을 호출
   onChange function은 input에 입력한 value를 토대로  
@@ -33,7 +36,6 @@ const Auth = () => {
       let data;
       if (newAccount) {
         //create account
-        const auth = getAuth();
         data = await createUserWithEmailAndPassword(
           authService,
           email,
@@ -41,13 +43,45 @@ const Auth = () => {
         );
       } else {
         //login
-        const auth = getAuth();
         data = await signInWithEmailAndPassword(authService, email, password);
       }
       console.log(data);
     } catch (error) {
       setError(alert(error.message));
     }
+  };
+  //setNewAccount를 호출, newAccount의 이전 값을 가져와서 반대되는 값을 반환
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const auth = getAuth();
+    //팝업 창을 사용한 로그인을 위하여 signInWithPopup 호출
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
   };
   return (
     <div>
@@ -68,11 +102,18 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "LogIn"} />
+        <input type="submit" value={newAccount ? "Create Account" : "Login"} />
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
