9d24ae8d39a6fb5dbae04114a88942cb0d3a1e58
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-12-17 21:30:41 +0900

    feat: Create Navigation.js, Profile.js and logout func

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,18 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exat path="/" element={<Home />}></Route>
+            <Route exat path="/profile" element={<Profile />}></Route>
           </>
         ) : (
           <Route exact path="/" element={<Auth />}></Route>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
 import React from "react";
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+export default () => {
+  /*react router의 useNavigate hook을 이용하여 
+    Logout 버튼을 클릭하여 onLogoutClick 호출 시 
+    Logout과 동시에 navigate를 통해 root 경로로 이동*/
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Logout</button>
+    </>
+  );
+};
