77d965eb35167191fac2745df646f94d36379baa
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-12-20 18:20:47 +0900

    feat: Create getSweets func

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,28 @@
 import { dbService } from "fbase";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import React, { useState } from "react";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  serverTimestamp,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [sweet, setSweet] = useState("");
+  const [sweets, setSweets] = useState([]);
+  const getSweets = async () => {
+    const dbSweets = await getDocs(collection(dbService, "sweets"));
+    dbSweets.forEach((document) => {
+      const sweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+      setSweets((prev) => [sweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getSweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await addDoc(collection(dbService, "sweets"), {
@@ -18,6 +37,7 @@ const Home = () => {
     } = event;
     setSweet(value);
   };
+  console.log(sweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -30,6 +50,13 @@ const Home = () => {
         />
         <input type="submit" value="Sweet" />
       </form>
+      <div>
+        {sweets.map((sweet) => (
+          <div key={sweet.id}>
+            <h4>{sweet.sweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
