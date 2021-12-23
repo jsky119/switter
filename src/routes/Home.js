df5b3ba708a235218e6945126a24725da55cd449
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-12-24 03:17:09 +0900

    refactor: Update unsubscribe code in useEffect

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -31,14 +31,27 @@ const Home = ({ userObj }) => {
       collection(dbService, "sweets"),
       orderBy("createdAt", "desc")
     );
-    onSnapshot(q, (snapshot) => {
-      const sweetArr = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+    /*listener로 snapshot을 사용 
+    onSnapshot은 db의 변화를 observing한다
+    db에 변화가 있으면 이 snapshot을 받아 map()을 이용, 배열로 만들며
+    sweets state에 배열을 set한다. 이를 Home에서 rendering하는 div component에서 사용함.
+    */
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const sweetArr = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
       setSweets(sweetArr);
     });
+    /*snapshot이 db를 계속 observing하므로 발생하는 memory leak를 방지하기 위해
+    component가 사라질 때 cleanup하는 함수 호출 */
+    return () => {
+      unsubscribe();
+    };
   }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     await addDoc(collection(dbService, "sweets"), {
